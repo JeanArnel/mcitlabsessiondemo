53cc8d32283ac38bb4a526627945bc71bb68ab4b
JeanArnel <168039393+JeanArnel@users.noreply.github.com>
2024-04-26 20:14:37 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,10 +1,22 @@
 locals{
   servicename="abracadabra"
   forum="abracadabramcit"
+  firstname="tamgho"
+  lastname="arnel"
+  city="Montreal"
 }
 output "print"{
     value = local.servicename
 }
 output "secondprint"{
     value = local.forum
+}
+output "thirdprint"{
+    value = local.firstname
+}
+output "fourthprint"{
+    value = local.lastname
+}
+output "fifthprint"{
+    value = local.city
 }
