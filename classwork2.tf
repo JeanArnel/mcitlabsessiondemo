2f9b555915f6e54622a209610472fab235c1b43e
JeanArnel <168039393+JeanArnel@users.noreply.github.com>
2024-05-03 19:38:16 -0400

    Update classwork2.tf

--- classwork2.tf
+++ classwork2.tf
@@ -5,3 +5,14 @@ default=[["a","b"],["c","d"],["e","f"]]
 output "simple_local_flattened_list"{
 value= flatten(var.simple_nested-list)
 }
+
+variable "nested_map"{
+type= map(map(string))
+default= {
+group1= {"name"= "Alice", "age"= "25" }
+group2= {"name"= "Bob", "age"= "30"}
+}
+}
+output "flatened_map"{
+value= flatten([for k,v in var.nested_map]:[for kk, w in v:{group= k, key=kk, value=w}])
+}
