186a3bb211ea3bc9d439ea73b4672875cc31e5f5
JeanArnel <168039393+JeanArnel@users.noreply.github.com>
2024-05-03 19:17:15 -0400

    Update classwork2.tf

--- classwork2.tf
+++ classwork2.tf
@@ -3,5 +3,5 @@ type= list(list(string))
 default=[["a","b"],["c","d"],["e","f"]]
 }
 output "simple_local_flattened_list"{
-simple_local_flattened_list= flatten(var.simple_nested-list)
+value= flatten(var.simple_nested-list)
 }
