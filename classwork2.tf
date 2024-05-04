8ef29dd2ea3fbde52920a5f9b9419daebace430f
JeanArnel <168039393+JeanArnel@users.noreply.github.com>
2024-05-03 20:03:42 -0400

    Update classwork2.tf

--- classwork2.tf
+++ classwork2.tf
@@ -20,10 +20,9 @@ value= flatten([for k,v in var.nested_map:[for kk, vv in v:{group= k, key=kk, va
 locals{
 characters=["luke", "yoda", "darth"]
 enemies_destroyed= [4252, 900, 200000056894]
-character_enemy_map= { for index,character in local.characters: character=> local.enemies_destroyed[index]
+output"character_enemy_map"{
+value= { for index,character in local.characters: character=> local.enemies_destroyed[index]
 }
-output{
-value= local.character_enemy_map
 }
 }
 
