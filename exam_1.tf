fca4e6566d4998000595d8fdca2ac1f3fe463a46
JeanArnel <168039393+JeanArnel@users.noreply.github.com>
2024-05-06 19:03:59 -0400

    Update exam_1.tf

--- exam_1.tf
+++ exam_1.tf
@@ -6,3 +6,7 @@ contains_word= contains(local.clouds, "azure")
 output "check_if" {
 value=local.contains_word? "azure is part of the cloud list": "azure is not part of the cloud list"
 }
+
+output "new_clouds_set"{
+new_clouds_set=>concat(local.clouds, ["alibaba", "ibm"])
+}
