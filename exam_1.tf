9777b2be8163b7cdc0e2d10e0ad58b64842c0f55
JeanArnel <168039393+JeanArnel@users.noreply.github.com>
2024-05-06 18:48:42 -0400

    Update exam_1.tf

--- exam_1.tf
+++ exam_1.tf
@@ -1,7 +1,8 @@
 //checking if "azure" is part of a string list in a local variable
 locals {
 clouds= ["azure","aws","gcp"] 
+contains_word= contains(local.clouds, "azure")
 }
 output "check_if" {
-value=local.contains(clouds, "azure")? "azure is part of the cloud list": "azure is not part of the cloud list"
+value=local.contains_word? "azure is part of the cloud list": "azure is not part of the cloud list"
 }
