91c2af7357877e40150fa3ff6447b6b3b66971e3
JeanArnel <168039393+JeanArnel@users.noreply.github.com>
2024-05-06 20:13:09 -0400

    Create kubernetes.tf

---
+++ kubernetes.tf
@@ -0,0 +1,7 @@
+locals{
+cluster_names= ["lknmdi876", "arcdst2014", "grdfauy5648", "sxczva64578", "nmcvbas879521"]
+}
+resource "azurerm_resource_group" "azureresourcegroup"{
+name= "MCIT_Lab_session"
+location= "canadacentral"
+}
