e08f6567035381e752a70f8ef58eb4c8b24ff272
JeanArnel <168039393+JeanArnel@users.noreply.github.com>
2024-05-08 19:27:56 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -6,17 +6,15 @@ name= "MCIT_Lab_session"
 location= "canadacentral"
 }
 
-
-/*
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
  
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  name                = "lknmdi876"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
   dns_prefix          = "exampleaks1"
  
   default_node_pool {
@@ -33,7 +31,94 @@ resource "azurerm_kubernetes_cluster" "example" {
     Environment = "Production"
   }
 }
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "arcdst2014"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "exampleaks2"
  
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+ 
+  identity {
+    type = "SystemAssigned"
+  }
+ 
+  tags = {
+    Environment = "Production"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "grdfauy5648"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "exampleaks3"
+ 
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+ 
+  identity {
+    type = "SystemAssigned"
+  }
+ 
+  tags = {
+    Environment = "Production"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "sxczva64578"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "exampleaks4"
+ 
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+ 
+  identity {
+    type = "SystemAssigned"
+  }
+ 
+  tags = {
+    Environment = "Production"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "nmcvbas879521"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "exampleaks5"
+ 
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+ 
+  identity {
+    type = "SystemAssigned"
+  }
+ 
+  tags = {
+    Environment = "Production"
+  }
+}
+
+
+
+ /*
 output "client_certificate" {
   value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
   sensitive = true
