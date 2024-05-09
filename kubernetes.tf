19c57999ee09f9a5d305335c8da8f0b32ddf505c
JeanArnel <168039393+JeanArnel@users.noreply.github.com>
2024-05-08 21:12:28 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -125,6 +125,12 @@ for_each            = {for cluster in var.cluster_name: cluster=>cluster}
   name                = "${var.prefix}cluster"
 location= "canadacentral"
 resource_group_name = azurerm_resource_group.azureresourcegroup.name
+
+ default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
 }
   
 
